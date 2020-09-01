97ad4e7b626546d317b463d125f3e145b27d3d5c
seiya <skkasans@gmail.com>
2020-09-02 07:58:11 +0900

    ユーザ登録機能のバリデーションを変更

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,8 +50,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'alpha_num', 'min:4', 'max:16', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users',
+            'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
