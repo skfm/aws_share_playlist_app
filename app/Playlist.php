33004fbd9c3379d0aad50f37e24f048c6a3f8f1c
seiya <skkasans@gmail.com>
2020-09-03 05:40:09 +0900

    usersテーブルとplaylistsテーブルのリレーションを追加

--- app/Playlist.php
+++ app/Playlist.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Playlist extends Model
 {
-    //
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo('App\User');
+    }
 }
