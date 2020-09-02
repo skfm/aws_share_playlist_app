49027d384041a9b652add2e5c1ca7e963ebbf6b2
seiya <skkasans@gmail.com>
2020-09-03 05:47:42 +0900

    プレイリスト投稿ページの仮作成

---
+++ resources/views/playlists/form.blade.php
@@ -0,0 +1,13 @@
+@csrf
+<div class="md-form">
+  <label>タイトル</label>
+  <input type="text" name="title" class="form-control" required value="{{ old('title') }}">
+</div>
+<div class="form-group">
+  <label></label>
+  <textarea name="description" required class="form-control" rows="16" placeholder="本文">{{ old('body') }}</textarea>
+</div>
+<div class="md-form">
+  <label>YouTubeのプレイリストURL</label>
+  <input type="text" name="url" class="form-control" required value="{{ old('url') }}">
+</div>
