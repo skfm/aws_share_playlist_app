a5217c123a7919483ccf022189de5af61d12d568
seiya <skkasans@gmail.com>
2020-09-03 05:23:26 +0900

    マイグレーションファイルを修正
    外部キー制約のエラーを修正

--- database/migrations/2020_09_03_045637_create_playlists_table.php
+++ database/migrations/2020_09_03_045637_create_playlists_table.php
@@ -18,7 +18,7 @@ class CreatePlaylistsTable extends Migration
             $table->string('title');
             $table->text('url');
             $table->text('description');
-            $table->bigInteger('user_id');
+            $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
