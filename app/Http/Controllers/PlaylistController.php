525601361765142b9f7f784d979af2aa91d3a63a
seiya <skkasans@gmail.com>
2020-09-06 13:04:51 +0900

    機能追加
    プレイリスト一覧ページの作成

--- app/Http/Controllers/PlaylistController.php
+++ app/Http/Controllers/PlaylistController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers;
 use App\Playlist;
 use App\Http\Requests\PlaylistRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PlaylistController extends Controller
 {
     public function __construct()
     {
         $this->authorizeResource(Playlist::class, 'playlist');
+    }
+
+    public function index(Playlist $playlist)
+    {
+        $user_id = Auth::user()->id;
+        $playlists = $playlist::where('user_id', $user_id)->get()->sortByDesc('created_at');
+        $user_name = Auth::user()->name;
+        return view('playlists.index', [
+            'playlists' => $playlists,
+            'name' => $user_name,
+        ]);
     }
 
     public function create()
@@ -22,7 +34,7 @@ class PlaylistController extends Controller
         $playlist->fill($request->all());
         $playlist->user_id = $request->user()->id;
         $playlist->save();
-        return redirect()->route('home');
+        return redirect()->route('playlists.index');
     }
 
     public function edit(Playlist $playlist)
@@ -33,13 +45,13 @@ class PlaylistController extends Controller
     public function update(PlaylistRequest $request, Playlist $playlist)
     {
         $playlist->fill($request->all())->save();
-        return redirect()->route('home');
+        return redirect()->route('playlists.index');
     }
 
     public function destroy(Playlist $playlist)
     {
         $playlist->delete();
-        return redirect()->route('home');
+        return redirect()->route('playlists.index');
     }
 
     public function show(Playlist $playlist)
---
+++ resources/views/playlists/index.blade.php
@@ -0,0 +1,35 @@
+@extends('app')
+
+@section('title', '記事一覧')
+
+@section('content')
+  @include('nav')
+  <div class="container">
+    @foreach($playlists as $playlist)
+      <div class="card mt-3">
+        <div class="card-body d-flex flex-row">
+          <i class="fas fa-user-circle fa-3x mr-1"></i>
+          <div>
+            <div class="font-weight-bold">
+              {{ $name }}
+            </div>
+            <div class="font-weight-lighter">
+              {{ $playlist->created_at->format('Y/m/d H:i') }}
+            </div>
+          </div>
+        </div>
+        <div class="card-body pt-0 pb-2">
+          <h3 class="h4 card-title">
+            {{ $playlist->title }}
+          </h3>
+          <div class="card-text">
+            {!! nl2br(e( $playlist->description )) !!}
+          </div>
+          <div class="card-text">
+            {{ $playlist->url }}
+          </div>
+        </div>
+      </div>
+    @endforeach
+  </div>
+@endsection
