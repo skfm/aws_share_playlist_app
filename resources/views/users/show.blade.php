01bcbb73f476275fc7f98ce1b0d9d9883796757d
seiya <skkasans@gmail.com>
2020-10-05 20:31:12 +0900

    ユーザー詳細ページの修正

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -13,8 +13,8 @@
       @include('playlists.card')
     @endforeach
 
-    <div class="">
-      <a class="nav-link text-muted active"
+    <div class="btn-wrap mt-3 mb-3">
+      <a type="button" class="d-flex justify-content-center btn btn-flat nav-link text-muted active"
              href="{{ route('users.allplaylists', [
               'name' => $user->name,
           ]) }}">
