737c1c15f3be59dadf1d63dbc7cb36a45fb750c3
seiya <skkasans@gmail.com>
2020-10-05 20:26:37 +0900

    ストックフォルダー一覧ページの修正

--- resources/views/stockfolders/index.blade.php
+++ resources/views/stockfolders/index.blade.php
@@ -4,13 +4,21 @@
 
 @section('content')
   @include('nav')
-  <div class="container">
+
+  <div class="container pb-3">
+    @include('users.user')
+
+    <div class="flderLink-wrap d-flex flex-row mt-3">
+      <a class="text-muted active" href="{{ route('stockfolders.create') }}">
+        フォルダを作成する
+      </a>
+      <a class="text-muted active" href="{{ route('stockfolders.index') }}">
+        フォルダ一覧
+      </a>
+    </div>
+
     @foreach($stock_folders as $stock_folder)
       @include('stockfolders.card')
     @endforeach
   </div>
-  <a class="nav-link text-muted active"
-             href="{{ route('stockfolders.create') }}">
-    フォルダを作成する
-  </a>
 @endsection
