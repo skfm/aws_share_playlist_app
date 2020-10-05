00c739c2fb5bae4ff72502f687621f8a39004c92
seiya <skkasans@gmail.com>
2020-10-05 20:19:28 +0900

    カテゴリー別の一覧ページの表示を調整
    •デザイン修正は未完了

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -4,20 +4,20 @@
 
 @section('content')
   @include('nav')
-  <div class="col-xs-12 col-sm-12 col-md-12">
-    <form action="{{ url("/categories/$category->title")}}">
-    <select name="sort">
-      <option value="new">新しい順</option>
-      <option value="old">古い順</option>
-      <option value="allsotck">ストックが多い順</option>
-    </select>
-    <button type="submit" class="btn btn-primary col-md-5">並び替え</button>
-    </form>
-  </div>
   <div class="container">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+      <form action="{{ url("/categories/$category->title")}}">
+      <select name="sort">
+        <option value="new">新しい順</option>
+        <option value="old">古い順</option>
+        <option value="allsotck">ストックが多い順</option>
+      </select>
+      <button type="submit" class="btn btn-primary col-md-5">並び替え</button>
+      </form>
+    </div>
     <div class="card mt-3">
-      <div class="card-body">
-        <h2 class="h4 card-title m-0">{{ $category->title }}</h2>
+      <div class="card-body d-flex align-items-center justify-content-between">
+        <h2 class="h4 card-title m-0">カテゴリー：{{ $category->title }}</h2>
         <div class="card-text text-right">
           {{ $category->playlists->count() }}件
         </div>
