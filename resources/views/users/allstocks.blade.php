211117ae2bb6f845944938ab931b0153fa2b15be
seiya <skkasans@gmail.com>
2020-10-05 20:30:48 +0900

    ユーザーストック一覧ページの修正

--- resources/views/users/allstocks.blade.php
+++ resources/views/users/allstocks.blade.php
@@ -4,24 +4,35 @@
 
 @section('content')
   @include('nav')
-  <div class="container">
-    <?php $i = 0; ?>
-    @foreach($playlists as $playlist)
-        @include('playlists.card')
-        <?php $i =  ++$i; ?>
-    @endforeach
+
+  <div class="container pb-3">
+    @include('users.user')
+
+    <div class="stocks-wrap mt-3">
+      <div class="flderLink-wrap d-flex flex-row">
+        <a class="text-muted active" href="{{ route('stockfolders.create') }}">
+          フォルダを作成する
+        </a>
+        <a class="text-muted active" href="{{ route('stockfolders.index') }}">
+          フォルダ一覧
+        </a>
+      </div>
+
+      @if($stock_folders)
+        <div class="folders-wrap d-flex flex-row">
+          @foreach($stock_folders as $stock_folder)
+            <a href="{{ route('stockfolders.show', ['stockfolder' => $stock_folder]) }}">
+              {{ $stock_folder->name }}
+            </a>
+          @endforeach
+        </div>
+      @endif
+
+      <?php $i = 0; ?>
+      @foreach($playlists as $playlist)
+          @include('playlists.card')
+          <?php $i =  ++$i; ?>
+      @endforeach
+    </div>
   </div>
-  @foreach($stock_folders as $stock_folder)
-    <a href="{{ route('stockfolders.show', ['stockfolder' => $stock_folder]) }}">
-      {{ $stock_folder->name }}
-    </a>
-  @endforeach
-  <a class="nav-link text-muted active"
-             href="{{ route('stockfolders.create') }}">
-    フォルダを作成する
-  </a>
-  <a class="nav-link text-muted active"
-             href="{{ route('stockfolders.index') }}">
-    フォルダ一覧
-  </a>
 @endsection
