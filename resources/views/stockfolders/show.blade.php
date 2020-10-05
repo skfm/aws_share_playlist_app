2d33d53bf0ee6743f9050603e88398b7440b678b
seiya <skkasans@gmail.com>
2020-10-05 20:27:06 +0900

    ストックフォルダー詳細ページの修正

--- resources/views/stockfolders/show.blade.php
+++ resources/views/stockfolders/show.blade.php
@@ -5,6 +5,17 @@
 @section('content')
   @include('nav')
   <div class="container">
+    @include('users.user')
+
+    <div class="card mt-3">
+      <div class="card-body d-flex align-items-center justify-content-between">
+        <h2 class="h4 card-title m-0">フォルダ：{{ $stockfolder->name }}</h2>
+        <div class="card-text text-right">
+          {{ $count }}件
+        </div>
+      </div>
+    </div>
+
     @foreach($playlists as $playlist)
       @include('playlists.card')
     @endforeach
