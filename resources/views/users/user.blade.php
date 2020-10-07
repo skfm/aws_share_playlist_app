5a4cc79b3601acaff6aec5af681768a435b3e2f7
seiya <skkasans@gmail.com>
2020-10-07 22:23:33 +0900

    ユーザ情報のテンプレートデザイン修正

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -1,20 +1,20 @@
-<div class="card mt-3 pl-3 pr-3">
-  <div class="card-body pb-0">
+<div class="card mt-3 pl-3 pr-3 userCard">
+  <div class="card-body">
     <div class="d-flex flex-row align-items-center justify-content-between">
       <div class="title-wrap d-flex flex-row align-items-center">
         @if ($user->image_path)
-        <img src="{{ asset('storage/avatar/' . $user->image_path) }}" style="width: 48px; height: 48px; border-radius: 50%; object-fit: cover;">
+        <img src="{{ asset('storage/avatar/' . $user->image_path) }}">
         @else
         <i class="fas fa-user-circle fa-3x"></i>
         @endif
         <h2 class="h5 card-title mb-0 ml-2">
-          <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
+          <a href="{{ route('users.show', ['name' => $user->name]) }}">
             {{ $user->name }}
           </a>
         </h2>
       </div>
       <div class="sns-wrap d-flex flex-row align-items-center">
-        <a href="{{ $user->insta_url }}" type="button" class="btn-floating btn-small btn-instas"><i class="fab fa-instagram"></i></a>
+        <a href="{{ $user->insta_url }}" type="button" class="btn-floating btn-small btn-insta"><i class="fab fa-instagram"></i></a>
 
         <a href="{{ $user->twitter_url }}" type="button" class="btn-floating btn-small btn-tw"><i class="fab fa-twitter"></i></a>
 
