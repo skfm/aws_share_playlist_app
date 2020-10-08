d4d66d73255112637f4bf727269fa3f0c9c50e62
seiya <skkasans@gmail.com>
2020-10-08 20:35:48 +0900

    ユーザーのストック一覧ページのデザインを修正

--- resources/views/users/allstocks.blade.php
+++ resources/views/users/allstocks.blade.php
@@ -10,23 +10,13 @@
 
     <div class="stocks-wrap mt-3">
       <div class="flderLink-wrap d-flex flex-row">
-        <a class="text-muted active" href="{{ route('stockfolders.create') }}">
+        <a href="{{ route('stockfolders.create') }}">
           フォルダを作成する
         </a>
-        <a class="text-muted active" href="{{ route('stockfolders.index') }}">
+        <a href="{{ route('stockfolders.index') }}">
           フォルダ一覧
         </a>
       </div>
-
-      @if($stock_folders)
-        <div class="folders-wrap d-flex flex-row">
-          @foreach($stock_folders as $stock_folder)
-            <a href="{{ route('stockfolders.show', ['stockfolder' => $stock_folder]) }}">
-              {{ $stock_folder->name }}
-            </a>
-          @endforeach
-        </div>
-      @endif
 
       <?php $i = 0; ?>
       @foreach($playlists as $playlist)
