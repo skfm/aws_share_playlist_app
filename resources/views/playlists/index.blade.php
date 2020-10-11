e075490e73b7c5ef652672258ab8d58051affa2d
seiya <skkasans@gmail.com>
2020-10-12 03:31:44 +0900

    トップページのカテゴリー部分の修正
    •アイコンの追加

--- resources/views/playlists/index.blade.php
+++ resources/views/playlists/index.blade.php
@@ -37,45 +37,53 @@
     <h3 class="categories-title">
       カテゴリー別にみる
     </h3>
-    <ul class="categories-list d-flex justify-content-between flex-wrap p-0">
-      <li class="categories-item col-6">
+    <ul class="categories-list d-flex justify-content-around flex-wrap p-0">
+      <li class="categories-item col-6 col-sm-3">
+        <i class="fas fa-tablet-alt"></i>
         <a href="{{ route('categories.show', ['title' => 'プログラミング']) }}">
           プログラミング
         </a>
       </li>
-      <li class="categories-item col-6 col-sm-3">
-        <a href="{{ route('categories.show', ['title' => 'お金']) }}">
-          お金
+      <li class="categories-item col-6 col-md-3">
+        <i class="fas fa-coins"></i>
+        <a href="{{ route('categories.show', ['title' => '副業']) }}">
+          副業
         </a>
       </li>
-      <li class="categories-item col-6 col-sm-3">
-        <a href="{{ route('categories.show', ['title' => '美容']) }}">
-          美容
-        </a>
-      </li>
-      <li class="categories-item col-6 col-sm-3">
+      <li class="categories-item col-6 col-md-3">
+        <i class="far fa-hospital"></i>
         <a href="{{ route('categories.show', ['title' => '健康']) }}">
           健康
         </a>
       </li>
-      <li class="categories-item col-6 col-sm-3">
-        <a href="{{ route('categories.show', ['title' => 'プログラミング']) }}">
-          プログラミング
-        </a>
-      </li>
-      <li class="categories-item col-6 col-sm-3">
-        <a href="{{ route('categories.show', ['title' => 'お金']) }}">
-          お金
-        </a>
-      </li>
-      <li class="categories-item col-6 col-sm-3">
+      <li class="categories-item col-6 col-md-3">
+        <i class="fas fa-vial"></i>
         <a href="{{ route('categories.show', ['title' => '美容']) }}">
           美容
         </a>
       </li>
-      <li class="categories-item col-6 col-sm-3">
-        <a href="{{ route('categories.show', ['title' => '健康']) }}">
-          健康
+      <li class="categories-item col-6 col-md-3">
+        <i class="fas fa-dumbbell"></i>
+        <a href="{{ route('categories.show', ['title' => 'トレーニング']) }}">
+          トレーニング
+        </a>
+      </li>
+      <li class="categories-item col-6 col-md-3">
+        <i class="fas fa-money-bill-wave-alt"></i>
+        <a href="{{ route('categories.show', ['title' => '投資']) }}">
+          投資
+        </a>
+      </li>
+      <li class="categories-item col-6 col-md-3">
+        <i class="fas fa-heart"></i>
+        <a href="{{ route('categories.show', ['title' => '恋愛']) }}">
+          恋愛
+        </a>
+      </li>
+      <li class="categories-item col-6 col-md-3">
+        <i class="far fa-grin-squint"></i>
+        <a href="{{ route('categories.show', ['title' => 'オモシロ動画']) }}">
+          オモシロ動画
         </a>
       </li>
     </ul>
