ad2ff1e8538ab661311f31f3f5f5662417cb22f8
seiya <skkasans@gmail.com>
2020-10-12 02:59:13 +0900

    カテゴリー一覧ページの修正

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -4,25 +4,31 @@
 
 @section('content')
   @include('nav')
-  <div class="container">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-      <form action="{{ url("/categories/$category->title")}}">
-      <select name="sort">
-        <option value="new">新しい順</option>
-        <option value="old">古い順</option>
-        <option value="allsotck">ストックが多い順</option>
-      </select>
-      <button type="submit" class="btn btn-primary col-md-5">並び替え</button>
-      </form>
-    </div>
-    <div class="card mt-3">
+  <div class="container mt-3 pb-3">
+    <div class="card resultNumber">
       <div class="card-body d-flex align-items-center justify-content-between">
-        <h2 class="h4 card-title m-0">カテゴリー：{{ $category->title }}</h2>
-        <div class="card-text text-right">
+        <h2 class="card-title m-0 resultNumber-title">カテゴリー：{{ $category->title }}</h2>
+        <div class="card-text text-right resultNumber-number">
           {{ $category->playlists->count() }}件
         </div>
       </div>
     </div>
+
+    <div class="formResult mt-3">
+      <form action="{{ url("/categories/$category->title")}}">
+      {{ csrf_field()}}
+      <div class="form-group">
+        <label>並び順</label>
+        <select name="sort" class="browser-default custom-select">
+          <option value="new">新しい順</option>
+          <option value="old">古い順</option>
+          <option value="allsotck">ストックが多い順</option>
+        </select>
+      </div>
+      <button type="submit" class="btn btn-search">並び替え</button>
+      </form>
+    </div>
+
     @foreach($playlists as $playlist)
       @include('playlists.card')
     @endforeach
