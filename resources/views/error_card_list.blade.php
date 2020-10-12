f1928ec9079da3ef897e7991031964715be421a8
seiya <skkasans@gmail.com>
2020-10-12 14:13:16 +0900

    バリデーションエラーの表示デザインの修正

--- resources/views/error_card_list.blade.php
+++ resources/views/error_card_list.blade.php
@@ -1,8 +1,8 @@
 @if ($errors->any())
-  <div class="card-text text-left alert alert-danger">
-    <ul class="mb-0">
+  <div class="card-text text-left alert alert-danger mt-3 validation">
+    <ul class="mb-0 validation-list">
       @foreach($errors->all() as $error)
-        <li>{{ $error }}</li>
+        <li class="validation-item">{{ $error }}</li>
       @endforeach
     </ul>
   </div>
