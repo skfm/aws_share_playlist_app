fbdb3ea54a3cdb70df609094f309f4941244586a
seiya <skkasans@gmail.com>
2020-10-23 03:35:18 +0900

    ユーザーページのSNSアイコンをURL登録された時のみ表示

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -2,7 +2,7 @@
   <div class="card-body">
     <div class="d-flex flex-row align-items-center justify-content-between">
       <div class="title-wrap d-flex flex-row align-items-center">
-        @if ($user->image_path)
+        @if($user->image_path)
         <img src="{{ asset('storage/avatar/' . $user->image_path) }}">
         @else
         <i class="fas fa-user-circle fa-3x"></i>
@@ -14,11 +14,17 @@
         </h2>
       </div>
       <div class="sns-wrap d-flex flex-row align-items-center">
-        <a href="{{ $user->insta_url }}" type="button" class="btn-floating btn-small btn-insta" target="_blank"><i class="fab fa-instagram"></i></a>
+        @if($user->insta_url)
+         <a href="{{ $user->insta_url }}" type="button" class="btn-floating btn-small btn-insta" target="_blank"><i class="fab fa-instagram"></i></a>
+        @endif
 
-        <a href="{{ $user->twitter_url }}" type="button" class="btn-floating btn-small btn-tw" target="_blank"><i class="fab fa-twitter"></i></a>
+        @if($user->twitter_url)
+          <a href="{{ $user->twitter_url }}" type="button" class="btn-floating btn-small btn-tw" target="_blank"><i class="fab fa-twitter"></i></a>
+        @endif
 
-        <a href="{{ $user->youtube_url }}" type="button" class="btn-floating btn-small btn-youtube" target="_blank"><i class="fab fa-youtube"></i></a>
+        @if($user->youtube_url)
+          <a href="{{ $user->youtube_url }}" type="button" class="btn-floating btn-small btn-youtube" target="_blank"><i class="fab fa-youtube"></i></a>
+        @endif
       </div>
     </div>
     <p class="card-text mt-2">
