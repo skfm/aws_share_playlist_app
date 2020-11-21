75a380d6442575db5614d9811de349535428284a
seiya <skkasans@gmail.com>
2020-11-21 17:30:09 +0900

    デザイン修正
    TOPページのカテゴリー部分でホバーエフェクトを追加

--- resources/views/playlists/index.blade.php
+++ resources/views/playlists/index.blade.php
@@ -40,51 +40,51 @@
     </h3>
     <ul class="categories-list d-flex justify-content-around flex-wrap p-0">
       <li class="categories-item col-6 col-sm-3">
-        <i class="fas fa-tablet-alt"></i>
         <a href="{{ route('categories.show', ['title' => 'プログラミング']) }}">
-          プログラミング
+          <i class="fas fa-tablet-alt"></i>
+          <span>プログラミング</span>
         </a>
       </li>
       <li class="categories-item col-6 col-md-3">
-        <i class="fas fa-coins"></i>
         <a href="{{ route('categories.show', ['title' => '副業']) }}">
-          副業
+          <i class="fas fa-coins"></i>
+          <span>副業</span>
         </a>
       </li>
       <li class="categories-item col-6 col-md-3">
-        <i class="far fa-hospital"></i>
         <a href="{{ route('categories.show', ['title' => '健康']) }}">
-          健康
+          <i class="far fa-hospital"></i>
+          <span>健康</span>
         </a>
       </li>
       <li class="categories-item col-6 col-md-3">
-        <i class="fas fa-vial"></i>
         <a href="{{ route('categories.show', ['title' => '美容']) }}">
-          美容
+          <i class="fas fa-vial"></i>
+          <span>美容</span>
         </a>
       </li>
       <li class="categories-item col-6 col-md-3">
-        <i class="fas fa-dumbbell"></i>
         <a href="{{ route('categories.show', ['title' => 'トレーニング']) }}">
-          トレーニング
+          <i class="fas fa-dumbbell"></i>
+          <span>トレーニング</span>
         </a>
       </li>
       <li class="categories-item col-6 col-md-3">
-        <i class="fas fa-money-bill-wave-alt"></i>
         <a href="{{ route('categories.show', ['title' => '投資']) }}">
-          投資
+          <i class="fas fa-money-bill-wave-alt"></i>
+          <span>投資</span>
         </a>
       </li>
       <li class="categories-item col-6 col-md-3">
-        <i class="fas fa-heart"></i>
         <a href="{{ route('categories.show', ['title' => '恋愛']) }}">
-          恋愛
+          <i class="fas fa-heart"></i>
+          <span>恋愛</span>
         </a>
       </li>
       <li class="categories-item col-6 col-md-3">
-        <i class="far fa-grin-squint"></i>
         <a href="{{ route('categories.show', ['title' => 'オモシロ動画']) }}">
-          オモシロ動画
+          <i class="far fa-grin-squint"></i>
+          <span>オモシロ動画</span>
         </a>
       </li>
     </ul>
