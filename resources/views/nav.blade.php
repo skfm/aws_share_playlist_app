72785012da6acad689a919e2bf7ebbeb9bb71d61
seiya <skkasans@gmail.com>
2020-11-26 07:26:42 +0900

    ヘッダーにカテゴリー検索機能を追加

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -18,7 +18,7 @@
 
     @auth
     <li class="nav-item">
-      <a class="nav-link" href="{{ route('playlists.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a>
+      <a class="nav-link" href="{{ route('playlists.create') }}"><i class="fas fa-pen mr-1"></i>投稿</a>
     </li>
     @endauth
 
@@ -59,6 +59,70 @@
           </div>
           <button type="submit" class="btn">検索</button>
           </form>
+      </div>
+    </div>
+
+    <div class="search nav-item dropdown">
+      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+         aria-haspopup="true" aria-expanded="false">
+         <i class="fas fa-th-list"></i>
+      </a>
+      <div class="search-wrap dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+        <div class="navCategories pt-3 pl-3 pr-3">
+          <h3 class="navCategories-title">
+            カテゴリー
+          </h3>
+          <ul class="navCategories-list d-flex justify-content-around flex-wrap p-0">
+            <li class="navCategories-item">
+              <a href="{{ route('categories.show', ['title' => 'プログラミング']) }}">
+                <i class="fas fa-tablet-alt"></i>
+                <span>プログラミング</span>
+              </a>
+            </li>
+            <li class="navCategories-item">
+              <a href="{{ route('categories.show', ['title' => '副業']) }}">
+                <i class="fas fa-coins"></i>
+                <span>副業</span>
+              </a>
+            </li>
+            <li class="navCategories-item">
+              <a href="{{ route('categories.show', ['title' => '健康']) }}">
+                <i class="far fa-hospital"></i>
+                <span>健康</span>
+              </a>
+            </li>
+            <li class="navCategories-item">
+              <a href="{{ route('categories.show', ['title' => '美容']) }}">
+                <i class="fas fa-vial"></i>
+                <span>美容</span>
+              </a>
+            </li>
+            <li class="navCategories-item">
+              <a href="{{ route('categories.show', ['title' => 'トレーニング']) }}">
+                <i class="fas fa-dumbbell"></i>
+                <span>トレーニング</span>
+              </a>
+            </li>
+            <li class="navCategories-item">
+              <a href="{{ route('categories.show', ['title' => '投資']) }}">
+                <i class="fas fa-money-bill-wave-alt"></i>
+                <span>投資</span>
+              </a>
+            </li>
+            <li class="navCategories-item">
+              <a href="{{ route('categories.show', ['title' => '恋愛']) }}">
+                <i class="fas fa-heart"></i>
+                <span>恋愛</span>
+              </a>
+            </li>
+            <li class="navCategories-item">
+              <a href="{{ route('categories.show', ['title' => 'オモシロ動画']) }}">
+                <i class="far fa-grin-squint"></i>
+                <span>オモシロ動画</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
 
